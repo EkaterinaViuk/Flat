e296b14a5f7cf5b530d25ad76c664618d15f63fc
EkaterinaViuk <katyaviuk@mail.ru>
2022-03-29 17:25:45 +0300

    555

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введи своё имя:");
-string name = Console.ReadLine();
-Console.WriteLine("В каком году ты родился?");
-int year = int.Parse(Console.ReadLine());
-Console.WriteLine($"Сейчас тебе {2022 - year} лет");
+Console.Write("Номер квартиры: ");
+double flat; 
+double.TryParse(Console.ReadLine(), out flat);
+Console.Write("Сколько этажей в подъезде: ");
+double floor;
+double.TryParse(Console.ReadLine(), out floor);
+Console.Write("Сколько квартир на этаже: ");
+double num;
+double.TryParse(Console.ReadLine(), out num);
+double ost = Math.Ceiling(flat/(num*floor)) - floor*num;
+double r = Math.Ceiling((flat - Math.Ceiling(flat / (floor * num)) * floor * num * ost) / num);
+Console.WriteLine($"Квартира находится в {Math.Ceiling(flat/(floor*num))} подъезде на {r} этаже");
